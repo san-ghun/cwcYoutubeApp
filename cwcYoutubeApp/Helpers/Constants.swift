4ada81e260fa8cc7e64d596f9db5b3810664e682
Sanghun <po4865+github_sanghun@gmail.com>
2022-01-18 18:10:46 +0900

    Add second UIViewController - Add second UIViewController in Main.storyboard to present selected video with title, date and description - Update ViewController.swift to prepare() segue a video date to DetailViewController - Add DetailViewController.swift to present video and detail information about the selected video - Use WebKit and YouTubeiOSPlayerHelper to play the presented YouTube video     - First, don't know why but embed and presenting Youtube Video with WebKit didn't work     - As alternative, added [YouTubeiOSPlayerHelper](https://github.com/youtube/youtube-ios-player-helper) through Swift Package and it plays video well     - Will try to find way to play and present video only with WebKit, or just remove WebKit part and use only the YouTubeiOSPlayerHelper

--- cwcYoutubeApp/Helpers/Constants.swift
+++ cwcYoutubeApp/Helpers/Constants.swift
@@ -14,5 +14,7 @@ struct Constants {
     static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     
     static var VIDEOCELL_ID = "VideoCell"
+    
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
 }
 
---
+++ cwcYoutubeApp/ViewControllers/DetailViewController.swift
@@ -0,0 +1,77 @@
+//
+//  DetailViewController.swift
+//  cwcYoutubeApp
+//
+//  Created by Sanghun Park on 2022/01/18.
+//
+
+import UIKit
+import WebKit
+import YouTubeiOSPlayerHelper
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var youtubePlayerView: YTPlayerView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    var video: Video?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Clear the fields
+        titleLabel.text = ""
+        dateLabel.text = ""
+        textView.text = ""
+        
+        // Check if there's a video
+        guard video != nil else { return }
+        
+        // Create the embed URL
+        let embedUrlString = Constants.YT_EMBED_URL + video!.videoId
+        
+        // Load it into the webview
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        // Load it into the Youtube player view
+        self.youtubePlayerView.load(withVideoId: video!.videoId)
+        
+        // Set the title
+        titleLabel.text = video!.title
+        
+        // Set the date
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = df.string(from: video!.published)
+        
+        // Set the description
+        textView.text = video!.description
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
